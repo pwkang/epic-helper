f7145ac5a39ca15ca557a9a45476d115f763320a
Wei Kang <porweikang@hotmail.com>
2023-05-06 15:31:12 +0800

    chore: initial setup

--- src/bot.ts
+++ src/bot.ts
@@ -1,22 +1,24 @@
 import {ClusterClient, getInfo} from 'discord-hybrid-sharding';
 import {Client} from 'discord.js';
+
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 const environment = process.env.NODE_ENV || 'development';
 
-const shards = environment === 'development' ? undefined : getInfo().SHARD_LIST;
+const shards = environment === 'development' ? 'auto' : getInfo().SHARD_LIST;
+
 const shardCount = environment === 'development' ? 1 : getInfo().TOTAL_SHARDS;
 
 const client: BotClient = new Client({
-    intents: [],
-    shardCount,
-    shards
+  intents: [],
+  shardCount,
+  shards,
 });
 
 if (environment === 'production') {
-    client.cluster = new ClusterClient(client); // initialize the Client, so we access the .broadcastEval()
+  client.cluster = new ClusterClient(client); // initialize the Client, so we access the .broadcastEval()
 }
 client.login(process.env.BOT_TOKEN).then(() => {
-    console.log('Logged in!');
+  console.log('Logged in!');
 });
