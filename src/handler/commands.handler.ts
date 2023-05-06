4ff673f45cb9bce89b6bdf725b7be679ec455644
Wei Kang <porweikang@hotmail.com>
2023-05-06 16:57:58 +0800

    fix: fix dynamic imports

--- src/handler/commands.handler.ts
+++ src/handler/commands.handler.ts
@@ -5,7 +5,7 @@ export default function loadPrefixCommands(client: BotClient) {
   readdirp('./src/commands', {fileFilter: '*.ts'}).on('data', async (entry: EntryInfo) => {
     const {fullPath} = entry;
     const file = await import(fullPath);
-    const command = file.default as PrefixCommand;
+    const command = file.default.default as PrefixCommand;
     client.commands.set(command.name, command);
   });
 }
