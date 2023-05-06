c6193977429460bc3a84516a82747ed839ec2bc2
Wei Kang <porweikang@hotmail.com>
2023-05-06 16:58:53 +0800

    fix: update prefix command root path

--- src/handler/commands.handler.ts
+++ src/handler/commands.handler.ts
@@ -2,7 +2,7 @@ import readdirp from 'readdirp';
 import type {EntryInfo} from 'readdirp';
 
 export default function loadPrefixCommands(client: BotClient) {
-  readdirp('./src/commands', {fileFilter: '*.ts'}).on('data', async (entry: EntryInfo) => {
+  readdirp('./src/commands/prefix', {fileFilter: '*.ts'}).on('data', async (entry: EntryInfo) => {
     const {fullPath} = entry;
     const file = await import(fullPath);
     const command = file.default.default as PrefixCommand;
