50322df4b38b02b1ab08d3fe28226fe711ac3b08
Wei Kang <porweikang@hotmail.com>
2023-05-07 01:05:45 +0800

    feat: add pet info constants

--- src/types/rpg.ts
+++ src/types/rpg.ts
@@ -36,3 +36,44 @@ export const RPG_FARM_SEED = {
   carrot: 'carrot',
   potato: 'potato',
 } as const;
+
+export const RPG_PET_TYPE = {
+  cat: 'cat',
+  dog: 'dog',
+  dragon: 'dragon',
+  pumpkinBat: 'Pumpkin Bat',
+  hamster: 'Hamster',
+  pinkFish: 'Pink Fish',
+  snowball: 'Snowball',
+  pony: 'Pony',
+  goldenBunny: 'Golden Bunny',
+  penguin: 'Penguin',
+  snowman: 'Snowman',
+  epicPanda: 'Epic Panda',
+  voidDog: 'VoiDog',
+} as const;
+
+export const RPG_PET_SKILL = {
+  fast: 'Fast',
+  happy: 'Happy',
+  clever: 'Clever',
+  digger: 'Digger',
+  lucky: 'Lucky',
+  timeTraveler: 'Time Traveler',
+  epic: 'EPIC',
+  ascended: 'ASCENDED',
+  fighter: 'FIGHTER',
+  perfect: 'PERFECT',
+};
+
+export const RPG_SPECIAL_PET_SKILL = {
+  pinkFish: 'Fisherfish',
+  pumpkinBat: 'Monster Hunter',
+  hamster: 'BOOSTER',
+  snowball: 'Gifter',
+  snowman: 'Gifter',
+  goldenBunny: 'Faster',
+  pony: 'Farmer',
+  epicPanda: 'Competitive',
+  penguin: 'Antarctician',
+} as const;
