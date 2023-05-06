9abb7fec5f21af1fee59c751c631f8b64ca4a3c2
Wei Kang <porweikang@hotmail.com>
2023-05-06 20:31:39 +0800

    chore: prevent load command that is not exported

--- src/handler/bot-events.handler.ts
+++ src/handler/bot-events.handler.ts
@@ -1,12 +1,14 @@
 import readdirp, {EntryInfo} from 'readdirp';
+import {Client} from 'discord.js';
 
-export default function loadBotEvents(client: BotClient) {
+export default function loadBotEvents(client: Client) {
   return new Promise((resolve, reject) => {
     readdirp('./src/events', {fileFilter: '*.ts'})
       .on('data', async (entry: EntryInfo) => {
         const {fullPath} = entry;
         const file = await import(fullPath);
         const command = file.default.default as BotEvent;
+        if (!command?.eventName) return;
         if (command.once) {
           client.once(command.eventName as keyof BotEvent, (...args) =>
             command.execute(client, ...args)
--- src/handler/commands.handler.ts
+++ src/handler/commands.handler.ts
@@ -1,14 +1,16 @@
 import type {EntryInfo} from 'readdirp';
 import readdirp from 'readdirp';
+import {Client} from 'discord.js';
 
-function loadPrefixCommands(client: BotClient) {
+function loadPrefixCommands(client: Client) {
   return new Promise((resolve, reject) => {
     readdirp('./src/commands/prefix', {fileFilter: '*.ts'})
       .on('data', async (entry: EntryInfo) => {
         const {fullPath} = entry;
         const file = await import(fullPath);
         const command = file.default.default as PrefixCommand;
-        client.commands.set(command.name, command);
+        if (!command.name) return;
+        client.prefixCommands.set(`${command.type}:${command.name}`, command);
       })
       .on('end', () => {
         resolve({});
@@ -16,13 +18,14 @@ function loadPrefixCommands(client: BotClient) {
   });
 }
 
-function loadSlashCommands(client: BotClient) {
+function loadSlashCommands(client: Client) {
   return new Promise((resolve, reject) => {
     readdirp('./src/commands/slash', {fileFilter: '*.ts'})
       .on('data', async (entry: EntryInfo) => {
         const {fullPath} = entry;
         const file = await import(fullPath);
         const command = file.default.default as SlashCommand;
+        if (!command.name) return;
         client.slashCommands.set(command.name, command);
       })
       .on('end', () => {
@@ -31,6 +34,6 @@ function loadSlashCommands(client: BotClient) {
   });
 }
 
-export default function loadCommands(client: BotClient) {
+export default function loadCommands(client: Client) {
   return Promise.all([loadPrefixCommands(client), loadSlashCommands(client)]);
 }
