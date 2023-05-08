64a37e80d9c5409b554fdcc2f7a43166903bb6e5
pwkang <porweikang@hotmail.com>
2023-05-08 23:08:14 +0800

    feat: update rpg donor status

--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -1,6 +1,6 @@
 import {mongoClient} from '../../services/mongoose/mongoose.service';
 import userSchema from './user.schema';
-import {IUser} from './user.type';
+import {IUser, RpgDonorTier} from './user.type';
 
 const dbUser = mongoClient.model<IUser>('user', userSchema);
 
@@ -63,4 +63,53 @@ export const isUserAccountExist = async (userId: string): Promise<boolean> => {
     userId,
   });
   return user > 0;
+};
+
+interface IUpdateRpgDonorTier {
+  userId: string;
+  tier: RpgDonorTier;
+}
+
+export const updateRpgDonorTier = async ({userId, tier}: IUpdateRpgDonorTier): Promise<void> => {
+  await dbUser.findOneAndUpdate(
+    {
+      userId,
+    },
+    {
+      $set: {
+        'config.donor': tier,
+      },
+    }
+  );
+};
+
+interface IUpdateRpgDonorPTier {
+  userId: string;
+  tier: RpgDonorTier | null;
+}
+
+export const updateRpgDonorPTier = async ({userId, tier}: IUpdateRpgDonorPTier): Promise<void> => {
+  await dbUser.findOneAndUpdate(
+    {
+      userId,
+    },
+    {
+      $set: {
+        'config.donorP': tier,
+      },
+    }
+  );
+};
+
+export const removeRpgDonorPTier = async (userId: string): Promise<void> => {
+  await dbUser.findOneAndUpdate(
+    {
+      userId,
+    },
+    {
+      $unset: {
+        'config.donorP': '',
+      },
+    }
+  );
 };
