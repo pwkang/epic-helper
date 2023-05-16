7fb7f8b09d1351db38dd383b2fd0d437fd95bb71
pwkang <porweikang@hotmail.com>
2023-05-17 00:10:25 +0800

    feat: update traded ruby amount

---
+++ src/lib/epic_rpg/commands/other/trade.ts
@@ -0,0 +1,50 @@
+import {Embed, Message, User} from 'discord.js';
+import {RPG_ITEMS} from '../../../../constants/rpg_items';
+
+interface IExtractTradedItems {
+  embed: Embed;
+  author: User;
+}
+
+type ITradedItem = {
+  [key in keyof typeof RPG_ITEMS]?: number;
+};
+
+export const extractTradedItems = ({embed, author}: IExtractTradedItems): ITradedItem => {
+  const items = embed.fields[0].value.split('\n');
+
+  const tradedItemName = Object.entries(RPG_ITEMS).find(([_, item]) =>
+    items[0].includes(item.replaceAll(' ', ''))
+  )?.[0] as keyof typeof RPG_ITEMS;
+  const tradeItemAmount = (items[0].match(/x[\d,]+/)?.[0] ?? 'x0')
+    .replaceAll('x', '')
+    .replaceAll(',', '');
+  const itemGetName = Object.entries(RPG_ITEMS).find(([_, item]) =>
+    items[1].includes(item.replaceAll(' ', ''))
+  )?.[0] as keyof typeof RPG_ITEMS;
+  const itemGetAmount = (items[1].match(/x[\d,]+/)?.[0] ?? 'x0')
+    .replaceAll('x', '')
+    .replaceAll(',', '');
+
+  return {
+    [tradedItemName]: -tradeItemAmount,
+    [itemGetName]: +itemGetAmount,
+  };
+};
+
+interface IIsRpgTrade {
+  author: User;
+  embed: Embed;
+}
+
+export const isRpgTrade = ({embed, author}: IIsRpgTrade) =>
+  embed.description?.includes('Our trade is done then') &&
+  embed.fields[0]?.value.includes(author.username);
+
+interface IIsNotEnoughItems {
+  message: Message;
+  author: User;
+}
+
+export const isNotEnoughItems = ({author, message}: IIsNotEnoughItems) =>
+  message.content.includes("you don't have enough") && message.mentions.has(author.id);
