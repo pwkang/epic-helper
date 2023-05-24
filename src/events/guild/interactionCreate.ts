b082b3ffab7c0395470f23cfcd3ed2063d4162b2
pwkang <porweikang@hotmail.com>
2023-05-24 22:33:53 +0800

    chore: update slash command typescript

--- src/events/guild/interactionCreate.ts
+++ src/events/guild/interactionCreate.ts
@@ -1,17 +1,17 @@
-import {Events, type Interaction} from 'discord.js';
+import {BaseInteraction, ChatInputCommandInteraction, Events, type Interaction} from 'discord.js';
 
 export default <BotEvent>{
   eventName: Events.InteractionCreate,
   once: false,
-  execute: async (client, interaction: Interaction) => {
+  execute: async (client, interaction: BaseInteraction) => {
     if (!interaction.guild || !interaction.isCommand()) return;
-    
+
     const command = client.slashCommands.find(
       (cmd) => cmd.builder.name === interaction.commandName
     );
 
     if (!command) return;
 
-    await command.execute(client, interaction);
+    await command.execute(client, interaction as typeof command.interactionType);
   },
 };
