e14846f9bd3aca7dcd5330700804cf3d66c171a3
pwkang <porweikang@hotmail.com>
2023-05-28 00:04:42 +0800

    feat: add adventure slash command

--- src/commands/prefix/epic_rpg/progress/adventure.ts
+++ src/commands/prefix/epic_rpg/progress/adventure.ts
@@ -1,39 +1,16 @@
 import {COMMAND_TYPE} from '../../../../constants/bot';
-import {createRpgCommandListener} from '../../../../lib/epic_rpg/createRpgCommandListener';
-import rpgAdventure, {
-  isRpgAdventureSuccess,
-} from '../../../../lib/epic_rpg/commands/progress/adventure';
-import {updateUserCooldown} from '../../../../models/user-reminder/user-reminder.service';
-import {RPG_COMMAND_TYPE} from '../../../../constants/rpg';
+import {rpgAdventure} from '../../../../lib/epic_rpg/commands/progress/adventure';
 
 export default <PrefixCommand>{
   name: 'rpgAdventure',
   commands: ['adventure', 'adv'],
   type: COMMAND_TYPE.rpg,
   execute: async (client, message) => {
-    const event = createRpgCommandListener({
-      channelId: message.channel.id,
+    rpgAdventure({
       author: message.author,
+      message,
       client,
-    });
-    if (!event) return;
-    event.on('content', (content) => {
-      if (isRpgAdventureSuccess({author: message.author, content})) {
-        rpgAdventure({
-          author: message.author,
-          client,
-          channelId: message.channel.id,
-          content,
-        });
-        event.stop();
-      }
-    });
-    event.on('cooldown', (cooldown) => {
-      updateUserCooldown({
-        userId: message.author.id,
-        type: RPG_COMMAND_TYPE.adventure,
-        readyAt: new Date(Date.now() + cooldown),
-      });
+      isSlashCommand: false,
     });
   },
 };
---
+++ src/commands/slash_other_bot/epic_rpg/adventure.ts
@@ -0,0 +1,16 @@
+import {OTHER_BOT_TYPE} from '../../../constants/bot';
+import {rpgAdventure} from '../../../lib/epic_rpg/commands/progress/adventure';
+
+export default <SlashCommandOtherBot>{
+  name: 'rpgAdventure',
+  bot: OTHER_BOT_TYPE.rpg,
+  commandName: ['adventure'],
+  execute: async (client, message, author) => {
+    rpgAdventure({
+      author,
+      message,
+      client,
+      isSlashCommand: true,
+    });
+  },
+};
