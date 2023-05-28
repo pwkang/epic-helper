73d842f48619d3aa52d717dfe09e98daf7ca738a
pwkang <porweikang@hotmail.com>
2023-05-28 23:06:26 +0800

    feat: add open slash command

--- src/commands/prefix/epic_rpg/other/open.ts
+++ src/commands/prefix/epic_rpg/other/open.ts
@@ -1,23 +1,16 @@
 import {COMMAND_TYPE} from '../../../../constants/bot';
-import {createRpgCommandListener} from '../../../../lib/epic_rpg/createRpgCommandListener';
-import rpgOpenLootbox, {isLootboxOpen} from '../../../../lib/epic_rpg/commands/other/open';
+import {rpgOpenLootbox} from '../../../../lib/epic_rpg/commands/other/open';
 
 export default <PrefixCommand>{
   name: 'rpgOpen',
   commands: ['open'],
   type: COMMAND_TYPE.rpg,
   execute: async (client, message) => {
-    const event = createRpgCommandListener({
+    rpgOpenLootbox({
       author: message.author,
       client,
-      channelId: message.channelId,
-    });
-    if (!event) return;
-    event.on('embed', (embed) => {
-      if (isLootboxOpen({embed, author: message.author})) {
-        rpgOpenLootbox({embed, author: message.author});
-        event.stop();
-      }
+      message,
+      isSlashCommand: false,
     });
   },
 };
