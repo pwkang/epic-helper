076b0d3248d10de237f727078316e02a30ea4ba9
pwkang <porweikang@hotmail.com>
2023-05-28 22:58:25 +0800

    feat: add craft slash command

--- src/commands/prefix/epic_rpg/other/craft.ts
+++ src/commands/prefix/epic_rpg/other/craft.ts
@@ -1,26 +1,20 @@
 import {COMMAND_TYPE} from '../../../../constants/bot';
 import {createRpgCommandListener} from '../../../../lib/epic_rpg/createRpgCommandListener';
-import rpgCraft, {isSuccessfullyCrafted} from '../../../../lib/epic_rpg/commands/other/craft';
+import rpgCraftSuccess, {
+  isSuccessfullyCrafted,
+  rpgCraft,
+} from '../../../../lib/epic_rpg/commands/other/craft';
 
 export default <PrefixCommand>{
   name: 'rpgCraft',
   commands: ['craft ruby sword', 'craft ruby armor', 'craft coin sword'],
   type: COMMAND_TYPE.rpg,
   execute: (client, message) => {
-    const event = createRpgCommandListener({
-      channelId: message.channel.id,
-      client,
+    rpgCraft({
       author: message.author,
+      message,
+      isSlashCommand: false,
+      client,
     });
-    if (!event) return;
-    event.on('content', async (content) => {
-      if (isSuccessfullyCrafted({content})) {
-        await rpgCraft({
-          author: message.author,
-          content,
-        });
-      }
-    });
-    event.on('embed', (embed) => {});
   },
 };
