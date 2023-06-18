39b69f988a52e4dd97d9ffc5f439e0999806f42c
pwkang <porweikang@hotmail.com>
2023-06-18 15:42:16 +0800

    feat: add user toggle slash

---
+++ src/commands/slash/toggle/subcommand/showUserToggle.ts
@@ -0,0 +1,22 @@
+import {IToggleSubcommand} from '../toggle.type';
+import {userService} from '../../../../models/user/user.service';
+import commandHelper from '../../../../lib/epic-helper/command-helper';
+import djsInteractionHelper from '../../../../lib/discord.js/interaction';
+
+export const showUserToggleSlash = async ({client, interaction}: IToggleSubcommand) => {
+  const userToggle = await userService.getUserToggle(interaction.user.id);
+  if (!userToggle) return;
+
+  const embed = commandHelper.toggle.getUserToggleEmbed({
+    isDonor: true,
+    author: interaction.user,
+    userToggle,
+  });
+  await djsInteractionHelper.replyInteraction({
+    client,
+    interaction,
+    options: {
+      embeds: [embed],
+    },
+  });
+};
--- src/utils/slash-commands-listing.ts
+++ src/utils/slash-commands-listing.ts
@@ -1,28 +1,42 @@
 import readdirp from 'readdirp';
 import {handlerRoot} from '../handler/on-start/constant';
 import {SlashCommandBuilder} from 'discord.js';
+import {importFiles} from './filesImport';
 
 interface ISlashCommand {
   name: string;
   builder: SlashCommandBuilder;
 }
 
-export const listSlashCommands = (): Promise<ISlashCommand[]> => {
+export const listSlashCommands = async (): Promise<ISlashCommand[]> => {
   const slashCommands: ISlashCommand[] = [];
-  return new Promise((resolve) => {
-    readdirp(`./${handlerRoot}/commands/slash`, {fileFilter: '*.ts'})
-      .on('data', async (entry) => {
-        const {fullPath} = entry;
-        const file = await import(fullPath);
-        const command = file.default.default as SlashCommand;
-        if (!command?.builder) return;
-        slashCommands.push({
-          name: command.name,
-          builder: command.builder,
-        });
-      })
-      .on('end', () => {
-        resolve(slashCommands);
-      });
+  const commands = await importFiles<SlashCommand>({
+    options: {fileFilter: '*.ts'},
+    path: `./${handlerRoot}/commands/slash`,
   });
+  commands.forEach((command) => {
+    if (!command?.builder) return;
+    slashCommands.push({
+      name: command.name,
+      builder: command.builder,
+    });
+  });
+  return slashCommands;
+
+  // return new Promise((resolve) => {
+  //   readdirp(`./${handlerRoot}/commands/slash`, {fileFilter: '*.ts'})
+  //     .on('data', async (entry) => {
+  //       const {fullPath} = entry;
+  //       const file = await import(fullPath);
+  //       const command = file.default.default as SlashCommand;
+  //       if (!command?.builder) return;
+  //       slashCommands.push({
+  //         name: command.name,
+  //         builder: command.builder,
+  //       });
+  //     })
+  //     .on('end', () => {
+  //       resolve(slashCommands);
+  //     });
+  // });
 };
