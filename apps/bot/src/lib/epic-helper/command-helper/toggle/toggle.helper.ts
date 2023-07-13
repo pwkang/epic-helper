c38317d054ce0e49bf4763987287a72e036e472c
pwkang <porweikang@hotmail.com>
2023-07-14 00:04:04 +0800

    feat: add toggle range selector

--- apps/bot/src/lib/epic-helper/command-helper/toggle/toggle.helper.ts
+++ apps/bot/src/lib/epic-helper/command-helper/toggle/toggle.helper.ts
@@ -5,6 +5,7 @@ import {Guild, User} from 'discord.js';
 import {toggleDisplayList} from './toggle.list';
 import {IGuild, IUser, IUserToggle} from '@epic-helper/models';
 import {PREFIX} from '@epic-helper/constants';
+import {logger} from '@epic-helper/utils';
 
 export interface IGetUpdateQuery {
   on?: string;
@@ -12,23 +13,23 @@ export interface IGetUpdateQuery {
   toggleInfo: IToggleEmbedsInfo[];
 }
 
-const regex1 = /^([a-z])([0-9]+)$/;
-const regex2 = /^([a-z])([0-9]+)([a-z])$/;
+const regexParent = /^([a-z])([0-9]+)$/;
+const regexChild = /^([a-z])([0-9]+)([a-z])$/;
+const regexRange = /^([a-z])([0-9]+)-([a-z])([0-9]+)$/;
+
+const filterKeyword = (keyword: string): string[] =>
+  keyword
+    .toLowerCase()
+    .split(' ')
+    .filter((item) => regexParent.test(item) || regexChild.test(item) || regexRange);
 
 const getUpdateQuery = <T>({on, off, toggleInfo}: IGetUpdateQuery): UpdateQuery<T> => {
-  const itemOn =
-    on
-      ?.toLowerCase()
-      .split(' ')
-      .filter((item) => regex1.test(item) || regex2.test(item))
-      .map((item) => findPath(item, toggleInfo)) ?? [];
-  const itemOff =
-    off
-      ?.toLowerCase()
-      .split(' ')
-      .filter((item) => regex1.test(item) || regex2.test(item))
-      .map((item) => findPath(item, toggleInfo)) ?? [];
-
+  const itemOn = on
+    ? filterKeyword(on).flatMap((item) => getPathsFromKeyword(item, toggleInfo))
+    : [];
+  const itemOff = off
+    ? filterKeyword(off).flatMap((item) => getPathsFromKeyword(item, toggleInfo))
+    : [];
   const query: Record<string, boolean> = {};
 
   for (const item of itemOn) {
@@ -45,24 +46,70 @@ const getUpdateQuery = <T>({on, off, toggleInfo}: IGetUpdateQuery): UpdateQuery<
   };
 };
 
-const findPath = (key: string, toggleInfo: IToggleEmbedsInfo[]): string | null => {
-  const _key = regex1.test(key) ? key.match(regex1) : regex2.test(key) ? key.match(regex2) : null;
-  if (!_key) return null;
-
-  // convert a -> 0, b -> 1, c -> 2
-  const index1 = _key[1].charCodeAt(0) - 97;
-  const index2 = parseInt(_key[2]) - 1;
-  const index3 = _key[3] ? _key[3].charCodeAt(0) - 97 : null;
-
-  const path = toggleInfo[index1]?.children[index2] ?? null;
-
-  if (!path) return null;
-
-  if (index3 !== null && path.children) {
-    return path.children[index3].path;
-  } else {
-    return path.path;
+const getPathsFromKeyword = (
+  key: string,
+  toggleInfo: IToggleEmbedsInfo[]
+): string | string[] | null => {
+  if (regexParent.test(key)) {
+    const _key = key.match(regexParent);
+    if (!_key) return null;
+    return findPath({
+      toggleInfo,
+      groupIndex: parseGroupIndex(_key[1]),
+      parentIndex: parseParentIndex(_key[2]),
+    });
+  } else if (regexChild.test(key)) {
+    const _key = key.match(regexChild);
+    if (!_key) return null;
+    return findPath({
+      toggleInfo,
+      groupIndex: parseGroupIndex(_key[1]),
+      parentIndex: parseParentIndex(_key[2]),
+      childIndex: parseChildIndex(_key[3]),
+    });
+  } else if (regexRange.test(key)) {
+    const _key = key.match(regexRange);
+    if (!_key) return null;
+    const fromGroupIndex = parseGroupIndex(_key[1]);
+    const fromParentIndex = parseParentIndex(_key[2]);
+    const toGroupIndex = parseGroupIndex(_key[3]);
+    const toParentIndex = parseParentIndex(_key[4]);
+    if (fromGroupIndex !== toGroupIndex) return null;
+    const items = [];
+    for (let i = fromParentIndex; i <= toParentIndex; i++) {
+      const path = findPath({
+        toggleInfo,
+        groupIndex: fromGroupIndex,
+        parentIndex: i,
+      });
+      if (path) items.push(path);
+    }
+    return items;
   }
+  return null;
+};
+
+const parseGroupIndex = (key: string): number => key.charCodeAt(0) - 97;
+const parseParentIndex = (key: string): number => parseInt(key) - 1;
+const parseChildIndex = (key: string): number => key.charCodeAt(0) - 97;
+
+interface IFindPath {
+  toggleInfo: IToggleEmbedsInfo[];
+  groupIndex: number;
+  parentIndex: number;
+  childIndex?: number;
+}
+
+const findPath = ({toggleInfo, groupIndex, parentIndex, childIndex}: IFindPath): string | null => {
+  const parent = toggleInfo[groupIndex]?.children[parentIndex] ?? null;
+
+  if (!parent) return null;
+
+  if (childIndex !== undefined) {
+    return parent.children?.[childIndex]?.path ?? null;
+  }
+
+  return parent.path;
 };
 
 interface IGetDonorToggleEmbed {
@@ -82,7 +129,7 @@ const getDonorToggleEmbed = ({userAccount, author}: IGetDonorToggleEmbed) => {
     })
     .setDescription(
       `**Syntax 1:** \`${PREFIX.bot}t <on/off> <ID> [ID] [ID]\` - turn on/off any settings
-      > *\`${PREFIX.bot}t on a1 a5 b3a\`*
+      > *\`${PREFIX.bot}t on a1 a5 b3a c2-c5\`*
       **Syntax 2:** \`${PREFIX.bot}t reset\` - reset all settings`
     );
 };
