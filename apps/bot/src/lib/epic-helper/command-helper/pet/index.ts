7ed99a73b815e373dc81aaf10df8c8cd2f15f76c
Pwkang <porweikang@hotmail.com>
2023-11-20 21:34:18 +0800

    enhance: send message to copy on pet catch (#101)

--- apps/bot/src/lib/epic-helper/command-helper/pet/index.ts
+++ apps/bot/src/lib/epic-helper/command-helper/pet/index.ts
@@ -1,5 +1,7 @@
 import {_petSummaryHelper} from './pet-summary';
+import {_petCatchHelper} from './pet-catch';
 
 export const _petHelper = {
   summary: _petSummaryHelper,
+  petCatch: _petCatchHelper,
 };
