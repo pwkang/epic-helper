e88ffeb97d96ff36c2d439f8f457d0c039e9d399
Pwkang <porweikang@hotmail.com>
2023-12-01 10:53:12 +0800

    fix: stop mongodb connection when cluster dead (#109)

--- apps/bot/src/lib/epic-helper/command-helper/cluster/is-cluster-active.ts
+++ apps/bot/src/lib/epic-helper/command-helper/cluster/is-cluster-active.ts
@@ -1,9 +1,28 @@
 import type {Client} from 'discord.js';
 import {redisCluster} from '../../../../services/redis/cluster.redis';
+import {mongoClient} from '@epic-helper/services';
+import {logger} from '@epic-helper/utils';
 
 export const _isClusterActive = async (client: Client) => {
   if (!client.cluster) return true;
+  if(!client.isReady()) return false;
   const clusterInfo = await redisCluster.findCluster(client.cluster?.id);
 
-  return clusterInfo?.activeSession === client.clusterSession;
+  const isActive = clusterInfo?.activeSession === client.clusterSession;
+
+  if(!isActive) {
+    try{
+      if(mongoClient.readyState === 1)
+        await mongoClient.close(true);
+    }catch(e){
+      logger({
+        clusterId: client.cluster?.id,
+        logLevel: 'warn',
+        variant: 'cluster',
+        message: 'MongoDB connection is already closed',
+      });
+    }
+  }
+
+  return isActive;
 };
