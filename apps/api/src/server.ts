b2f2e46367138f07f21b1a39754dd6105e173bad
pwkang <porweikang@hotmail.com>
2023-12-10 00:25:30 +0800

    feat: add api to update donor

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,19 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
-import loadRoutes from './handler/routes.handler';
 import {logger} from '@epic-helper/utils';
+import {healthCheckController} from './routes/health-check';
+import {patreonController} from './routes/patreon';
+import {redisClient} from '@epic-helper/services';
 
 dotenv.config();
 
 const API_PORT = process.env.API_PORT || 3000;
 
 export const startServer = async () => {
+  await redisClient.connect();
+
   const app = express();
   app.use(bodyParser.json());
 
-  // app.use(morgan('combined'));
-  app.use('/', await loadRoutes());
+  app.get('/', healthCheckController.get);
+
+  app.post('/patreon-pledge', patreonController.post);
 
   app.listen(API_PORT, () => {
     logger({
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,10 @@ module.exports = {
       script: './apps/bot/dist/index.js',
       time: true,
     },
+    {
+      name: 'epic-helper-api',
+      script: './apps/api/dist/index.js',
+      time: true,
+    },
   ],
 };
