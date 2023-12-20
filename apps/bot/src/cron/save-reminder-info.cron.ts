3c6ad2cc7cd25a514beac10926eb98196f49e49f
Pwkang <porweikang@hotmail.com>
2023-12-20 10:27:35 +0800

    chore: update sync schedule (#126)

--- apps/bot/src/cron/save-reminder-info.cron.ts
+++ apps/bot/src/cron/save-reminder-info.cron.ts
@@ -2,7 +2,7 @@ import commandHelper from '../lib/epic-helper/command-helper';
 
 export default <CronJob>{
   name: 'save-reminder-info',
-  expression: '*/15 * * * *',
+  expression: '0 6 * * *',
   clusterId: 0,
   cronOptions: {},
   execute: async () => {
