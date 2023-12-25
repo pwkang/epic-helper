0ff417f9d37ddb5330adf50260bcf1ba9732b6fb
pwkang <porweikang@hotmail.com>
2023-12-25 21:57:12 +0800

    chore: change restart mode

--- apps/bot/src/lib/epic-helper/command-helper/cluster/is-cluster-active.ts
+++ apps/bot/src/lib/epic-helper/command-helper/cluster/is-cluster-active.ts
@@ -1,11 +1,5 @@
 import type {Client} from 'discord.js';
-import {redisCluster} from '@epic-helper/services';
 
 export const _isClusterActive = async (client: Client) => {
-  if (!client.cluster) return true;
-  if (!client.isReady()) return false;
-
-  const clusterInfo = await redisCluster.findCluster(client.cluster?.id);
-
-  return clusterInfo?.activeSession === client.clusterSession;
+  return !client.cluster || client.isReady();
 };
