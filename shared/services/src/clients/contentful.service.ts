913bd105aeaa5229bd65360cf008842beca97fc9
Pwkang <porweikang@hotmail.com>
2024-05-18 14:22:07 +0800

    fix: fix bot unable to start without contentful (#143)

--- shared/services/src/clients/contentful.service.ts
+++ shared/services/src/clients/contentful.service.ts
@@ -1,6 +1,14 @@
 import * as contentful from 'contentful';
 
-export const contentfulService = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE_ID ?? '',
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? '',
+const {CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN} = process.env;
+
+const hasContentfulCredentials = !!CONTENTFUL_SPACE_ID && !!CONTENTFUL_ACCESS_TOKEN;
+
+export const contentfulService = hasContentfulCredentials && contentful.createClient({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  space: process.env.CONTENTFUL_SPACE_ID,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
